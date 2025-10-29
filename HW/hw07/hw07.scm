7a6577b8f0b7dbcb9abad01c3cee23710b78e177
waterpineman <2776950076@qq.com>
2025-10-29 20:35:55 +0800

    hw07 complement

--- HW/hw07/hw07.scm
+++ HW/hw07/hw07.scm
@@ -1,15 +1,23 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp)
+  (if (= exp 0)
+      1
+      (if (even? exp)
+          (pow (square base) (/ exp 2))
+          (* base (pow base (- exp 1))))))
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________)
+      (let (
+               (y (repeatedly-cube (- n 1) x)))
         (* y y y))))
 
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) 
+    (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) 
+    (car  (cddr s)))
--- Lab/lab09/lab09.scm
+++ Lab/lab09/lab09.scm
@@ -1,10 +1,19 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+    (cond
+        ((< num1 num2) -1)
+        ((= num1 num2) 0)
+        ((> num1 num2)1)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+    (lambda (x)(+ x num)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+    (lambda (x)(f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n) 
+        (if (= n 0)
+            (lambda (x)x)
+            (composed f (repeat f (- n 1)))))
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +25,6 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) 
+    (if (= 0 (modulo a b))b
+        (gcd b (modulo a b))))
