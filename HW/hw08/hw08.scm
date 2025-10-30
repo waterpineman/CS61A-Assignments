9007d02c8af1066326f14a122a2b24d63f27abfd
waterpineman <2776950076@qq.com>
2025-10-30 18:58:06 +0800

    hw08 complement

--- HW/hw08/hw08.scm
+++ HW/hw08/hw08.scm
@@ -1,7 +1,42 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s) 
+        (define (ascendings pre s)
+            (if (= (length s) 0)
+                #t
+                (if (> pre (car s))
+                        #f
+                        (ascendings (car s) (cdr s)))))
+        
+            (if (= (length s) 0)
+                #t
+                (ascendings (car s) (cdr s))))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s) 
+    (if (= (length s) 0)
+        '()
+        (if (pred (car s))
+                (cons (car s) (my-filter pred (cdr s)))
+                (my-filter pred (cdr s)))))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+       (cond
+           ((null? lst1) lst2)
+           ((null? lst2) lst1)
+           (else 
+                 (cons (car lst1) (interleave lst2 (cdr lst1))))
+            ))
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s) 
+    (if (null? s)
+        '()
+        (if  (aloned (car s) (cdr s))
+        (cons (car s) (no-repeats (cdr s)))
+        (no-repeats (cdr s)))
+    )
+    )
+
+(define (aloned n s)
+        (cond
+            ((null? s) #t)
+            ((= n (car s)) #f)
+            (else (aloned n (cdr s))))
+)
